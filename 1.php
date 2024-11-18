d05d390af172e709bff99c1333fd610a16feb77b
Tendou-BakiBaki <75526020+Tendou-BakiBaki@users.noreply.github.com>
2024-11-18 17:13:06 +0500

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,23 @@
+<?php
+    $time_start = microtime(true);
+
+    // Спим некоторое время
+    usleep(100);
+
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+
+  echo "Ничего не делал $time секунд\n";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
